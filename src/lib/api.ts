814c814aa6a039e9cf9b560f5e17903df270dca8
miracle boniface <udohmiracle23@gmail.com>
2025-10-14 00:56:18 +0100

    repo updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:5000/api'
-const baseURL = 'https://nexcapital-backend.vercel.app/api/'
+const baseURL = 'https://nexcapital-backend.vercel.app/api'
 
 const api = axios.create({
   baseURL, // your NestJS backend
