72bd9dc7611058c3bfe20bb3cc59b48cae0c8ac9
otobong23 <113153105+otobong23@users.noreply.github.com>
2026-02-21 20:20:49 +0000

    repo updated

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -193,16 +193,18 @@ const Dashboard = () => {
               <p className="text-xs text-gray-400">2 new this week</p>
             </CardContent>
           </Card>
-            : <Card className="glass border-white/10">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium text-gray-400">Welcome Bonus</CardTitle>
-                <TrendingUp className="h-4 w-4 text-blue-500" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold text-white">${user.wallet.bonus ? user.wallet.bonus.toLocaleString() : (3200).toLocaleString()}</div>
-                <p className="text-xs text-gray-400">Starter Bonus</p>
-              </CardContent>
-            </Card>}
+            : <a href="/dahboard/deposit/" className="no-underline">
+              <Card className="glass border-white/10">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium text-gray-400">Welcome Bonus</CardTitle>
+                  <TrendingUp className="h-4 w-4 text-blue-500" />
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold text-white">${user.wallet.bonus ? user.wallet.bonus.toLocaleString() : (3200).toLocaleString()}</div>
+                  <p className="text-xs text-gray-400">Starter Bonus</p>
+                </CardContent>
+              </Card>
+            </a>}
         </div>
 
         {/* Market Chart */}
